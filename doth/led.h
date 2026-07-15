4468a1bb94a37b231093178a3ff7677b3fbe3736
sebasdv <sebasdv@gmail.com>
2026-07-15 15:07:54 -0400

    feat: LEDs sin GPIO bajo PIKO_GAMEPI13 (pines compartidos con botones)

--- doth/led.h
+++ doth/led.h
@@ -7,30 +7,37 @@ class LED {
  public:
   void Init(uint8_t gpio_) {
     gpio = gpio_;
+#if !PIKO_GAMEPI13
     gpio_init(gpio);
     gpio_set_dir(gpio, GPIO_OUT);
+    gpio_put(gpio, 0);
+#endif
     state = 0;
     dim_i = 0;
-    gpio_put(gpio, 0);
   }
 
   uint8_t Val() { return dim; }
 
   void Update() {
     dim_i++;
+#if !PIKO_GAMEPI13
     if (dim_i < dim) {
       gpio_put(gpio, 1);
     } else {
       gpio_put(gpio, 0);
     }
+#endif
   }
 
   void Set(bool on) {
+#if !PIKO_GAMEPI13
     if (on) {
       gpio_put(gpio, 1);
     } else {
       gpio_put(gpio, 0);
     }
+#endif
+    dim = on ? 255 : 0;
   }
 
   void SetDim(uint16_t dim_) {
